b4b1793a589788da2674eef1e8278f2ade4c41d6
张小云 <160196654+hi-zcy@users.noreply.github.com>
2025-02-16 17:37:37 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hpf',
+    version='0.3.0',
+    author='Chenyun Zhang',
+    author_email='"Chenyun Z." <zcy@hi-zcy.com>',
+    description='A high precision floating-point arithmetic module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/yourusername/high-precision-float',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    python_requires='>=3.0',
+    install_requires=[],  
+)
